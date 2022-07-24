94b83e4bbd40165cd510292ff8ad410e9c366339
pedromattar42 <pedromattar42@gmail.com>
2022-07-24 11:20:43 -0300

    primeiro commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './heroe.model';
+const HEROES: Hero[] = [
+  {id:1, name: 'Wolverine'},
+  {id:2, name: 'Wolverine'},
+  {id:3, name: 'Wolverine'},
+  {id:4, name: 'Wolverine'},
+  {id:5, name: 'Wolverine'},
+  {id:6, name: 'Wolverine'},
+  {id:7, name: 'Wolverine'},
+  {id:8, name: 'Wolverine'},
+  {id:9, name: 'Wolverine'},
+  {id:10, name: 'Wolverine'},
+  {id:11, name: 'Wolverine'},
+  {id:12, name: 'Wolverine'},
+  {id:13, name: 'Wolverine'},
+  {id:14, name: 'Wolverine'},
+  {id:15, name: 'Wolverine'}
+
+]
